e92b16206e2885ef9023f3106465b7cb31c0f881
Mariya Thomas <martho1172001@gmail.com>
2023-08-21 10:54:37 +0530

    change in upload file

--- src/pages/asset_management/assetUpload/index.tsx
+++ src/pages/asset_management/assetUpload/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { read, utils } from 'xlsx';
+// import { read, utils } from 'xlsx';
 import TitleBar from '../../../components/TitleBar/TitleBar';
 import './styles.css';
 
@@ -12,27 +12,27 @@ const UploadExcel = () => {
 
   const handleUpload = (e) => {
     e.preventDefault();
-    if (selectedFile) {
-      const reader = new FileReader();
+    // if (selectedFile) {
+    //   const reader = new FileReader();
 
-      reader.onload = (e) => {
-        const data = e.target.result;
-        const workbook = read(data, { type: 'array' });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const json = utils.sheet_to_json(worksheet);
+    //   reader.onload = (e) => {
+    //     const data = e.target.result;
+    //     const workbook = read(data, { type: 'array' });
+    //     const sheetName = workbook.SheetNames[0];
+    //     const worksheet = workbook.Sheets[sheetName];
+    //     const json = utils.sheet_to_json(worksheet);
+    //     console.log(json);
+    //     const filteredData = json.map(({ name, serial_no, subcategoryId }) => ({
+    //       name,
+    //       serial_no,
+    //       subcategoryId
+    //     }));
 
-        console.log(json);
-        const filteredData = json.map(({ name, serial_no, subcategoryId }) => ({
-          name,
-          serial_no,
-          subcategoryId
-        }));
-
-        console.log(filteredData);
-      };
-      reader.readAsArrayBuffer(selectedFile);
-    }
+    //     console.log(filteredData);
+    //   };
+    //   reader.readAsArrayBuffer(selectedFile);
+    // }
+    console.log(selectedFile);
   };
 
   return (
